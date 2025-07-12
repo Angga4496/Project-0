a7f481e7354f222cc2bc7f534e290214acab1aae
Angga <Anggalabola44@gmail.com>
2025-07-12 14:13:23 +0700

    excel_to_sql

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import cred
 df = pd.read_excel(r"C:\Users\ASUS\Downloads\FSI-2023-DOWNLOAD.xlsx")
 print(df.head())
 
-
+engine = db.create_engine("postgresql" + "://" + cred.user + ":" + cred.password + "@localhost:5432/testdb")
 
 
 #excel_csv=df.to("excel_csv_geany")
-#excel_sql=df.to_sql("excel_sql")
+excel_sql=df.to_sql("excel_sql",con=engine)
