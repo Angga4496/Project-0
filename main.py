16e92ba7283820aba29a452d628d621b9c4d6e16
Angga <Anggalabola44@gmail.com>
2025-07-12 21:57:15 +0700

    convert excel to sql using url

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import sqlalchemy as db
-import cred
+import makeurl
 
 #convert excel to csv
 df = pd.read_excel(r"C:\Users\ASUS\Downloads\FSI-2023-DOWNLOAD.xlsx")
 print(df.head())
 
-engine = db.create_engine("postgresql" + "://" + cred.user + ":" + cred.password + "@localhost:5432/testdb")
+#convert excel to sql
+engine = db.create_engine(makeurl.url_object)
 
-
-#excel_csv=df.to("excel_csv_geany")
-excel_sql=df.to_sql("excel_sql",con=engine)
+excel_sql=df.to_sql("excel_sql_2",con=engine)
