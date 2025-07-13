fc630e2edfb808a18897cec981253e0076c084c8
Angga <Anggalabola44@gmail.com>
2025-07-13 10:25:50 +0700

    Excel_to_SQL with credentials from different folder

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import sqlalchemy as db
+import sys
+sys.path.append('c:\\Users\\ASUS\\OneDrive\\Documents\\python project\\credentials')
 import makeurl
 
-#convert excel to csv
+print(makeurl.url_object)
+print("path")
+print(sys.path)
+
+#convert excel to dataframe
 df = pd.read_excel(r"C:\Users\ASUS\Downloads\FSI-2023-DOWNLOAD.xlsx")
-print(df.head())
 
-#convert excel to sql
+##convert excel to csv
+##excel_csv = df.to_csv("excel_csv")
+
+##convert excel to sql
+
+
 engine = db.create_engine(makeurl.url_object)
+excel_sql=df.to_sql("excel_sql_ultimate",con=engine)
 
-excel_sql=df.to_sql("excel_sql_2",con=engine)
