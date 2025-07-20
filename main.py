6e3cf31e24bcd5aed018db924d43aa15bb4ba14a
Angga <Anggalabola44@gmail.com>
2025-07-20 09:49:27 +0700

    final commit

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ df = pd.read_excel(r"C:\Users\ASUS\Downloads\FSI-2023-DOWNLOAD.xlsx")
 excel_csv = df.to_csv("excel_csv_tes")
 
 ##convert excel to sql
-#engine = db.create_engine(makeurl.url_object)
-#excel_sql=df.to_sql("excel_sql_ultimate",con=engine)
+engine = db.create_engine(makeurl.url_object)
+excel_sql=df.to_sql("excel_sql_ultimate",con=engine)
